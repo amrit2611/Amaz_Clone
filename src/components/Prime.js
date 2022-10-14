ab31cfc63df1220aabbeb3c61aa60a86cb20bb98
amrit2611 <81375472+amrit2611@users.noreply.github.com>
2022-10-14 12:38:45 +0530

    added component and media folder

---
+++ src/components/Prime.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Prime.css';
+
+
+function Prime() {
+  return (
+    <>
+      <h1>Prime</h1>
+    </>
+  )
+}
+
+export default Prime
---
+++ src/components/Returns.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Returns.css';
+
+
+function Returns() {
+  return (
+    <>
+      <h1>Returns</h1>
+    </>
+  )
+}
+
+export default Returns
